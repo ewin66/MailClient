c43ef1f086cb0335dedb78853dd6f263c0bc140a
svlcode <vio.slaniceanu@gmail.com>
2016-03-03 18:56:46 +0200

    First commit
    Uploaded initial DevExpress sources.

---
+++ sources/DevExpress.MailClient.Win/LanguageSelectorForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.MailClient.Win.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraSplashScreen;
+
+namespace DevExpress.MailClient.Win {
+    public partial class LanguageSelectorForm :XtraForm {
+        public LanguageSelectorForm() {
+            InitializeComponent();
+        }
+
+        frmMain mainForm;
+        private void simpleButton1_Click(object sender, EventArgs e) {
+            Hide();
+            SplashScreenManager.ShowForm(null, typeof(ssMain), true, true, false, 1000);
+            mainForm = new frmMain();
+            mainForm.ShowDialog();
+            Close();
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e) {
+            Hide();
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ar");
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ar");
+            Properties.Resources.Culture = System.Threading.Thread.CurrentThread.CurrentCulture;   //TODO
+            SplashScreenManager.ShowForm(null, typeof(ssMain), true, true, false, 1000);
+            mainForm = new frmMain();
+            mainForm.ShowDialog();
+            Close();
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e) {
+            Hide();
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("fa");
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("fa");
+            Properties.Resources.Culture = System.Threading.Thread.CurrentThread.CurrentCulture;   //TODO
+            SplashScreenManager.ShowForm(null, typeof(ssMain), true, true, false, 1000);
+            mainForm = new frmMain();
+            mainForm.ShowDialog();
+            Close();
+        }
+
+        private void simpleButton4_Click(object sender, EventArgs e) {
+            Hide();
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("he");
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("he");
+            Properties.Resources.Culture = System.Threading.Thread.CurrentThread.CurrentCulture;   //TODO
+            SplashScreenManager.ShowForm(null, typeof(ssMain), true, true, false, 1000);
+            mainForm = new frmMain();
+            mainForm.ShowDialog();
+            Close();
+        }
+    }
+}
---
+++ sources/DevExpress.MailClient.Win/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraSplashScreen;
+using DevExpress.Skins;
+using DevExpress.Data.Filtering;
+using DevExpress.MailClient.Win.Forms;
+using System.Drawing;
+using System.Threading;
+using DevExpress.XtraEditors;
+
+namespace DevExpress.MailClient.Win {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] arguments) {
+            DataHelper.ApplicationArguments = arguments;
+            DevExpress.Utils.LocalizationHelper.SetCurrentCulture(DataHelper.ApplicationArguments);
+            DevExpress.UserSkins.BonusSkins.Register();
+            DevExpress.Utils.AppearanceObject.DefaultFont = new Font("Segoe UI", 8);
+            DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Office 2013");
+            SkinManager.EnableFormSkins();
+            EnumProcessingHelper.RegisterEnum<TaskStatus>();
+            UnpackHelper.Unpack();
+            //SplashScreenManager.ShowForm(null, typeof(ssMain), true, true, false, 1000);
+            
+
+            //System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ar-SA");
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ar-SA");
+            //Properties.Resources.Culture = System.Threading.Thread.CurrentThread.CurrentCulture;   //TODO
+            //WindowsFormsSettings.RightToLeft = Utils.DefaultBoolean.True;
+            //WindowsFormsSettings.RightToLeftLayout = Utils.DefaultBoolean.True;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LanguageSelectorForm());
+        }
+    }
+}
